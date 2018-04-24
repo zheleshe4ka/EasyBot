c538965c468e5d291c1bdd561287cf5495e4e8d4
Albert Alov <albert.alov@ncloudtech.ru>
2018-04-24 18:53:53 +0300

    EasyBot create

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import requests
-import datetime
 
 
 class BotHandler:
@@ -26,50 +25,6 @@ class BotHandler:
 
         if len(get_result) > 0:
             last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
+            return True, last_update
 
-        return last_update
-
-
-token = "589219887:AAH_L72CCkQlIJ21AGTqkPWNbs3x88hyx7Y"
-greet_bot = BotHandler(token)
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
-now = datetime.datetime.now()
-
-
-def main():
-    new_offset = None
-    today = now.day
-    hour = now.hour
-
-    while True:
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+        return False, 0
